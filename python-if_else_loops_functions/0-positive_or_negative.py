6fe4d4a8431e9fa9a34725ca6fc8ada06483f4e6
hashthat <samalberts73@gmail.com>
2024-03-05 21:29:45 -0600

    fizzbuzz!

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 
 if number > 0:
-    print(f"is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print(f"is zero")
+    print(f"{number} is zero")
 elif number < 0:
-    print(f"is negative")
+    print(f"{number} is negative")
--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1, 101):
+    for x in range(101):
         if x % 3 == 0 and x % 5 == 0:
-            print(f"{x - 1}: FizzBuzz", end="\n")
+            print(f"{x}: FizzBuzz", end="\n")
         elif x % 3 == 0:
-            print(f"{x - 1}: Fizz", end="\n")
+            print(f"{x}: Fizz", end="\n")
         elif x % 5 == 0:
-            print(f"{x - 1}: Buzz", end="\n")
+            print(f"{x}: Buzz", end="\n")
         else:
-            print(f"{x - 1}: ", end="\n")
+            print(f"{x}: ", end="\n")
 
 # Test the function
 fizzbuzz()
