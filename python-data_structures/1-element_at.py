35e77312ee4995937ba166d5d1a5721c77bd410c
hashthat <samalberts73@gmail.com>
2024-04-06 19:38:23 -0500

    python3 syntax to shebang

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+function retrieves an element from a list
+"""
+
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
